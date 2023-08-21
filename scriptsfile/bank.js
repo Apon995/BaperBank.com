040718130111f018344c1fdb8252d8461f34beb5
apon995 <shofikulsamz995@gmail.com>
2023-08-21 20:28:05 +0600

    'second-commit'

--- scriptsfile/bank.js
+++ scriptsfile/bank.js
@@ -1,56 +1,57 @@
 const Depositnow = () => {
-    let depositinputfield = getValueInputfrombankpage("depositInput");
-    if(isNaN(depositinputfield)){
-        document.getElementById('depositerror').innerText ="amount type kor joldi!"
-        setTimeout(() => {
-            document.getElementById('depositerror').innerText = '';
-        }, 5000);
+  let depositinputfield = getValueInputfrombankpage("depositInput");
+  if (isNaN(depositinputfield)) {
+    document.getElementById("depositerror").innerText =
+      "amount type kor joldi!";
+    setTimeout(() => {
+      document.getElementById("depositerror").innerText = "";
+    }, 5000);
 
-        return;
-    }
-    let currentdeposit = getvalueofinnerText("currentdeposit");
-    let currentbalance = getvalueofinnerText("currentbalance");
+    return;
+  }
+  let currentdeposit = getvalueofinnerText("currentdeposit");
+  let currentbalance = getvalueofinnerText("currentbalance");
 
-    let settotalbalance = currentbalance + depositinputfield;
-    let settotal = currentdeposit + depositinputfield;
+  let settotalbalance = currentbalance + depositinputfield;
+  let settotal = currentdeposit + depositinputfield;
 
-    setAmount("currentdeposit", settotal);
-    setAmount("currentbalance", settotalbalance);
+  setAmount("currentdeposit", settotal);
+  setAmount("currentbalance", settotalbalance);
+  SaveDeposit("currentdeposit");
+  SaveTotal("currentbalance");
 };
 
-
 const WithdrawNow = () => {
-    let Withdrawinputfield = getValueInputfrombankpage('withdrawInput')
-    if(isNaN(Withdrawinputfield)){
-        document.getElementById('errormsg').innerText ="amount type kor joldi!"
-        setTimeout(() => {
-            document.getElementById('errormsg').innerText = '';
-        }, 5000);
+  let Withdrawinputfield = getValueInputfrombankpage("withdrawInput");
+  if (isNaN(Withdrawinputfield)) {
+    document.getElementById("errormsg").innerText = "amount type kor joldi!";
+    setTimeout(() => {
+      document.getElementById("errormsg").innerText = "";
+    }, 5000);
 
-        return;
-    }
-    let currentWithdraw = getvalueofinnerText('withdrawAmount');
-    let total = currentWithdraw + Withdrawinputfield;
-    let currentbalance = getvalueofinnerText("currentbalance");
-    if (Withdrawinputfield <= currentbalance) {
-        let currentbalancetotal = currentbalance - Withdrawinputfield;
-        setAmount('currentbalance', currentbalancetotal);
-        setAmount('withdrawAmount', total)
+    return;
+  }
+  let currentWithdraw = getvalueofinnerText("withdrawAmount");
+  let total = currentWithdraw + Withdrawinputfield;
+  let currentbalance = getvalueofinnerText("currentbalance");
+  if (Withdrawinputfield <= currentbalance) {
+    let currentbalancetotal = currentbalance - Withdrawinputfield;
+    setAmount("currentbalance", currentbalancetotal);
+    setAmount("withdrawAmount", total);
+    Savewithdraw("withdrawAmount");
+    SaveTotal("currentbalance");
+  } else {
+    document.getElementById("errormsg").innerText =
+      "Tor baper bank e Atto taka nai !";
 
-    }
-    else {
-        document.getElementById('errormsg').innerText ="Tor baper bank e Atto taka nai !"
+    setTimeout(() => {
+      document.getElementById("errormsg").innerText = "";
+    }, 5000);
+  }
+};
 
-        setTimeout(() => {
-            document.getElementById('errormsg').innerText = '';
-        }, 5000);
-    }
-}
+const togglenav = (event) => {
+  event.name = event.name == "menu" ? "close" : "menu";
 
-
-
-const togglenav=(event)=>{
-    event.name = event.name == "menu"?"close":"menu";
-
-    document.querySelector('.opacity-0').classList.toggle('opacity-100');
-}
+  document.querySelector(".opacity-0").classList.toggle("opacity-100");
+};
--- scriptsfile/functions.js
+++ scriptsfile/functions.js
@@ -1,40 +1,62 @@
-// const letters = '0123456789ABCDEF';
-//       let color = '#';
-
-//       for (let i = 0; i < 6; i++) {
-//         color += letters[Math.floor(Math.random() * 16)];
-//     }
-// console.log(color);
-
-// login-field-input-value-access
 const getValueInputfieldfromlogin = (fieldId) => {
-    let value = document.getElementById(fieldId).value;
+  let value = document.getElementById(fieldId).value;
 
-    return value;
+  return value;
 };
 
 //bank page value access ;
 
 const getValueInputfrombankpage = (Elementid) => {
-    let value = document.getElementById(Elementid).value;
-    document.getElementById(Elementid).value = "";
-    value = parseInt(value);
-    
-    return value;
+  let value = document.getElementById(Elementid).value;
+  document.getElementById(Elementid).value = "";
+  value = parseInt(value);
+
+  return value;
 };
 
-
 const getvalueofinnerText = (innerId) => {
-    let innervalue = document.getElementById(innerId).innerText;
-    innervalue = parseInt(innervalue);
+  let innervalue = document.getElementById(innerId).innerText;
+  innervalue = parseInt(innervalue);
 
-    return innervalue;
-}
+  return innervalue;
+};
 
-const setAmount = (setelementid, totalset)=>{
-    let setamount = document.getElementById(setelementid);
-    setamount.innerText = totalset; 
+const setAmount = (setelementid, totalset) => {
+  let setamount = document.getElementById(setelementid);
+  setamount.innerText = totalset;
 
-    return setamount;
+  return setamount;
+};
 
-}
+const SaveDeposit = (elementId) => {
+  let deposit = document.getElementById(elementId).innerText;
+  deposit = parseFloat(deposit);
+  localStorage.setItem("depositBalance", deposit);
+};
+const Savewithdraw = (elementId) => {
+  let withdraw = document.getElementById(elementId).innerText;
+  withdraw = parseFloat(withdraw);
+  localStorage.setItem("withdrawBalance", withdraw);
+};
+const SaveTotal = (elementId) => {
+  let total = document.getElementById(elementId).innerText;
+  total = parseFloat(total);
+  localStorage.setItem("TotalBalance", total);
+};
+
+const getDeposit = () => {
+  document.getElementById("currentdeposit").innerText =
+    localStorage.getItem("depositBalance");
+};
+const getWithdraw = () => {
+  document.getElementById("withdrawAmount").innerText =
+    localStorage.getItem("withdrawBalance");
+};
+const getTotal = () => {
+  document.getElementById("currentbalance").innerText =
+    localStorage.getItem("TotalBalance");
+};
+
+getDeposit();
+getWithdraw();
+getTotal();
